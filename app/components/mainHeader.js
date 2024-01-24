531a11cee2551d89899e27b9e97e29ae6ee2c687
Šimon Žanta <simon.zanta@protonmail.com>
2024-01-24 01:29:51 +0100

    refactor(components): add correct paths for images

--- app/components/mainHeader.js
+++ app/components/mainHeader.js
@@ -6,7 +6,7 @@ export default function MainHeader(){
     return (
         <section className='h-screen'>
           <div className='relative'>
-            <img src='/background/keyboard.jpg' className='absolute gradient-mask-b-0 filter blur-sm hue-rotate-[-30deg] aspect-video'></img>
+            <img src='./background/keyboard.jpg' className='absolute gradient-mask-b-0 filter blur-sm hue-rotate-[-30deg] aspect-video'></img>
           </div>
           <div className='absolute flex flex-col p-5 lg:pt-5 pt-10 lg:p-32 gap-14'>
             <MainTitle text={"šimon"} noUnderline={true}/>
