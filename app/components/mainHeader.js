cab8eb435b333ebf5fdf7f12f0a46ee760e575dd
Šimon Žanta <simon.zanta@protonmail.com>
2024-01-24 11:46:16 +0100

    refactor(components:mainHeader): replace img with Image

--- app/components/mainHeader.js
+++ app/components/mainHeader.js
@@ -1,12 +1,13 @@
 import NavBar from './universal/navBar';
 import MainTitle from './font/mainTitle';
+import Image from 'next/image'
 
 export default function MainHeader(){
 
     return (
         <section className='h-screen'>
           <div className='relative'>
-            <img src='./background/keyboard.jpg' className='absolute gradient-mask-b-0 filter blur-sm hue-rotate-[-30deg] aspect-video'></img>
+            <Image src="/personalPortfolio/background/keyboard.jpg" width={0} height={0} sizes="100vw" className='h-auto w-full absolute gradient-mask-b-0 filter blur-sm hue-rotate-[-30deg] aspect-video invisible lg:visible'/>
           </div>
           <div className='absolute flex flex-col p-5 lg:pt-5 pt-10 lg:p-32 gap-14'>
             <MainTitle text={"šimon"} noUnderline={true}/>
