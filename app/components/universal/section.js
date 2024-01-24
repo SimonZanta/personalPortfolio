7497f3d645f2b6c846c53108a244d6267f6bb88e
Šimon Žanta <simon.zanta@protonmail.com>
2024-01-24 12:47:18 +0100

    feat(components:section): make section component more accesible for phone

--- app/components/universal/section.js
+++ app/components/universal/section.js
@@ -1,6 +1,6 @@
 export default function Section({children, fullFlexPage = true, reverseFlex = false, colLayout = false}){
     return(
-        <section className='min-h-screen mx-5 lg:mx-32 relative'>
+        <section className='min-h-[75vh] md:min-h-screen mx-5 lg:mx-32 relative'>
             {children}
         </section>
     )
