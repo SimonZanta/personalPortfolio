b4e34ca5bf27dea07ef0223b99402c62af2eb8d7
Šimon Žanta <simon.zanta@protonmail.com>
2024-01-24 12:48:01 +0100

    feat(components:mainHeader): better display of text on mobile

--- app/components/mainHeader.js
+++ app/components/mainHeader.js
@@ -9,7 +9,7 @@ export default function MainHeader(){
           <div className='relative'>
             <Image src="/personalPortfolio/background/keyboard.jpg" width={0} height={0} sizes="100vw" className='h-auto w-full absolute gradient-mask-b-0 filter blur-sm hue-rotate-[-30deg] aspect-video invisible lg:visible'/>
           </div>
-          <div className='absolute flex flex-col p-5 lg:pt-5 pt-10 lg:p-32 gap-14'>
+          <div className='absolute flex flex-col p-5 lg:pt-5 pt-10 lg:p-32 gap-14 top-1/4 -translate-y-1/4 md:top-0 md:-translate-y-0'>
             <MainTitle text={"šimon"} noUnderline={true}/>
             <MainTitle text={"tech"} noUnderline={true}/>
             <MainTitle text={"ventures"} noUnderline={true}/>
