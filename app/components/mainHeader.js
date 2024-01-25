66c2423ceba8f43f527f4392cf6be6d1e20880d1
Šimon Žanta <simon.zanta@protonmail.com>
2024-01-26 00:28:22 +0100

    refactor(app): make small changes to title background

--- app/components/mainHeader.js
+++ app/components/mainHeader.js
@@ -7,7 +7,8 @@ export default function MainHeader(){
     return (
         <section className='h-screen'>
           <div className='relative'>
-            <Image src="/personalPortfolio/background/keyboard.jpg" width={0} height={0} sizes="100vw" className='h-auto w-full absolute gradient-mask-b-0 filter blur-sm hue-rotate-[-30deg] aspect-video invisible lg:visible'/>
+            <div className='absolute h-screen gradient-mask-b-0'></div>
+            <Image src="/personalPortfolio/background/obstract.png" width={0} height={0} sizes="100vw" className='h-screen w-auto absolute invisible gradient-mask-b-0 lg:visible'/>px
           </div>
           <div className='absolute flex flex-col p-5 lg:pt-5 pt-10 lg:p-32 gap-14 top-1/4 -translate-y-1/4 md:top-0 md:-translate-y-0'>
             <MainTitle text={"šimon"} noUnderline={true}/>
