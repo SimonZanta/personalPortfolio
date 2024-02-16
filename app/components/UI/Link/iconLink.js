7e6e41ed276b1079bc67ab2271aacd219e93d6e0
Šimon Žanta <simon.zanta@protonmail.com>
2024-02-16 02:06:20 +0100

    feat(components:iconLink): add icon animation on hover

--- app/components/UI/Link/iconLink.js
+++ app/components/UI/Link/iconLink.js
@@ -3,14 +3,14 @@ import Image from 'next/image';
 export default function IconLink({link="/personalPortfolio", icon, isSmall=false}){
     //TODO create animation for hover
     return(
-        <a href={link} target="_blank">
+        <a href={link} target="_blank" className={'pr-4' + (isSmall ? "" : "pl-4")}>
             <Image
             src={"/personalPortfolio/icons/" + icon}
             width={0}
             height={0}
             alt={"icon"}
             sizes="100vw"
-            className={"pr-4  " + (isSmall ? "w-14" : "pl-4 w-20 h-20 md:w-32 md:h-32")}/>
+            className={" hover:animate-shake origin-center " + (isSmall ? "w-10" : "w-14 h-14 md:w-24 md:h-24") }/>
         </a>
     )
 }
