7b059b7d91fe75b59f38bc8a13d94946f1da71f0
Šimon Žanta <simon.zanta@protonmail.com>
2024-03-02 13:28:40 +0100

    feat(UI:headerText): add link to header component

--- app/components/UI/headerText.js
+++ app/components/UI/headerText.js
@@ -1,28 +1,35 @@
 'use client'
 import { useInView } from 'react-intersection-observer'
  
-export default function MainTitle({text, noUnderline}) {
+//component description
+//used to create heading, applies underline animation by default if component is in view
+
+//options
+//text - string, text to be displayed
+//noUnderline - boolean, if true, text wont be underlined and wont have animation
+
+export default function MainTitle({text, noUnderline, hasHoverEffect}) {
 
     const [ref, inView] = useInView({treshold: 1})
+    return(
+            <h1
+            ref={ref}
+            className={
+            'uppercase text-lg lg:text-xl tracking-wide ' + 
+            'overflow-hidden relative ' + 
+            (inView ? 'after:animate-underline max-w-fit ' : '') +
+            (noUnderline ? '' : 'after:underline-text-base ') + 
+            (hasHoverEffect ? "block hover:scale-105 text-link " : "")}
+            >
+                {text}
+            </h1>
+    )
+}
 
-    if(noUnderline){
-        return (
-            <h1 className="uppercase text-lg lg:text-xl tracking-wide">{text}</h1>
-          )
-    }else{
-        
-
-        return(
-                <h1
-                ref={ref}
-                className={
-                'uppercase text-lg lg:text-xl tracking-wide max-w-fit ' +
-                'overflow-hidden relative ' +
-                'after:underline-text-base ' +  
-                (inView ? 'after:animate-underline' : '')}
-                >
-                    {text}
-                </h1>
-        )
-    }
+export function MainTitleLink({text}){
+    return(
+            <a href="./personalPortfolio/projects">
+                <MainTitle text={text} hasHoverEffect={true}/>
+            </a>
+    )
 }
