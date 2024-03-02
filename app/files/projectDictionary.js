617af435199fceaf701acc195edae9b4e94f8073
Šimon Žanta <simon.zanta@protonmail.com>
2024-03-02 13:27:36 +0100

    add(files): add two new dictionaries

---
+++ app/files/projectDictionary.js
@@ -0,0 +1,12 @@
+export const projectDict = [
+    {
+        title: "GGS",
+        displayTechnologies: ["HTML", "CSS"],
+        link: "https://github.com/SimonZanta/GGS",
+        image: "GGS.png"
+    },
+]
+
+export const templateDict = [
+    projectDict.find((project) => project.title == "GGS")
+]
---
+++ app/files/skillDictionary.js
@@ -0,0 +1,6 @@
+export const skillDict = 
+    {
+        'Languages': ["JS", "Java", "HTML", "CSS"],
+        'Frameworks': ["React", "Node.js", "Spring Boot"],
+        'Design': ["Figma"]
+    }
