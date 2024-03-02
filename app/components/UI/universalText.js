342aa3d68e0665f93a5c2d7f839d4d4c8e9839fe
Šimon Žanta <simon.zanta@protonmail.com>
2024-03-02 13:29:37 +0100

    refactor(UI:univbersalText): add small improvements to text- typos, colors

--- app/components/UI/universalText.js
+++ app/components/UI/universalText.js
@@ -1,7 +1,6 @@
 'use client'
 import { useInView } from 'react-intersection-observer'
 
-export default function TextFont({text, isSmaller, isUppercase, isUnderlined, isHidden, hasHowerEffect}){
 //component description
 //universal text used all throughout app
 
@@ -12,15 +11,17 @@ export default function TextFont({text, isSmaller, isUppercase, isUnderlined, is
 //isUnderlined - boolean, checks if text should have underline
 //isHidden - boolean, hides text
 //hasHoverEffect - boolean, if chcked, text will scale up little bit
+
+export default function TextFont({text, isSmall, isUppercase, isUnderlined, isHidden, hasHoverEffect}){
     const [ref, inView] = useInView({treshold: 1})
     return(
         <span ref={ref} className={
-            (!isSmaller ? "md:text-base text-[25px] " : "text-sm ") +
+            (!isSmall ? "md:text-base text-[25px] " : "text-sm ") +
             (isUppercase ? "uppercase " : "") + 
             (isUnderlined ? "block relative tracking-wide max-w-fit after:underline-text-base " : "") + 
             (inView & isUnderlined ? "after:animate-underline " : "") + 
             (isHidden ? "hidden " : "") + 
-            (hasHowerEffect ? "block hover:scale-105 " : "")
+            (hasHoverEffect ? "block hover:scale-105 text-link " : "")
             }>
             {text}
         </span>
